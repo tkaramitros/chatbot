98421471f8f89b425a2ab09460a677d835fa3d05
mazhs1992 <75184007+mazhs1992@users.noreply.github.com>
2022-03-16 14:00:09 +0200

    Update mongoose.js

--- back/src/db/mongoose.js
+++ back/src/db/mongoose.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose')
 
 
-
 const generateData = require('../test/test');
 
 mongoose.connect('mongodb+srv://admin:admin@chatbot-test-shared.48emm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},()=>{
   console.log("Connected to Database")
   //generateData(20) //-->> to generate fake data
-
-const validator = require('validator')
-
-mongoose.connect('mongodb+srv://admin:admin@chatbot-test-shared.48emm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},()=>{
-  console.log("Connected to Database")
-
 })
-
-    
