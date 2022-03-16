cf338160c9588da2df8f09cc6b58aa9070e02ea6
mazhs1992 <75184007+mazhs1992@users.noreply.github.com>
2022-03-16 13:55:31 +0200

    Update app.js

--- back/src/app.js
+++ back/src/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,28 +5,11 @@ const cors = require('cors');
 const multer = require('multer');
 const morgan = require('morgan');
 
-const express = require ('express')
-const app = express() 
-cors = require('cors')
-
-require('./db/mongoose')
-
-const morgan= require('morgan')
-const bodyParser = require('body-parser')
-
-
-
-//routers
-const userRouter = require('./routers/user')
-const postRouter = require('./routers/post')
-
-
 
 require('./db/mongoose');
 
 //middleware
 app.use(morgan('dev')); //shows to the console what is send to the server
-
 
 // parse application/json
 app.use(bodyParser.json())
@@ -36,23 +18,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors());
-
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
-
-//CORS??????
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","*")
-    res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    if(req.method === 'OPTIONS'){
-        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE,GET")
-        return res.status(200).json({})
-    }
-    next()
-})
 
 
 
