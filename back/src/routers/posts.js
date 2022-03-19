e003ca071c6a0c568a8bb5c4975342212920f311
Evangelos Mazis <mazhs1992@hotmail.com>
2022-03-19 17:40:21 +0200

    Pagination Sorting

--- back/src/routers/posts.js
+++ back/src/routers/posts.js
@@ -2,17 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const paginate = require('express-paginate');
 
 const fs = require('fs');
-
 const Post = require('../models/Post');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 const multer = require('multer');
-
-
-
-
+const generateData = require('../test/test');
 
 //##########################     CREATE A POST - WITH IMAGE ##################
 const upload = multer({
@@ -28,30 +25,28 @@ const upload = multer({
 	}
 });
 
+//router.post('/', upload.array('images'), async (req, res) => {
+	// upload images
 router.post('/', upload.single('image'), async (req, res) => {
-	// upload images 
-	//router.post('/', upload.single('image'), async (req, res) => {
-	//const buf = [];
-	//req.files.forEach((element) => buf.push(element.buffer));
-	//console.log(req.file)
-	var picture
-	if (req.file){
-		 picture = req.file.buffer
+	// const buf = [];
+	// req.files.forEach((element) => buf.push(element.buffer));
+	// console.log(req.file)
+	var picture;
+	if (req.file) {
+		picture = req.file.buffer;
 	}
 	
 	const post = new Post({
 		_id: new mongoose.Types.ObjectId(),
 		title: req.body.title,
 		description: req.body.description,
-		location: req.body.location,
+		city: req.body.location,
 		price: req.body.price,
 		size: req.body.size,
 		propType: req.body.propType,
 		buyOrRent: req.body.buyOrRent,
 		//images: buf
-		image:picture
-		
-		
+		image: picture
 	});
 
 	try {
@@ -62,54 +57,64 @@ router.post('/', upload.single('image'), async (req, res) => {
 	}
 });
 
-
-//get: /localhost:4000/post
 //##########################     GET All POSTS  ##################
 router.get('/', async (req, res) => {
-	const reqQuery = {...req.query}
-	let queryStr = JSON.stringify(req.query)
-	queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g,(match)=>`$${match}`)	
+	//SORT
+	let x;
+	if (req.query.sort) {
+		let sortby = req.query.sort;		
+		x=sortby	
+	}else{
+		x='createdAt'
+	}
+	const reqQuery = { ...req.query };
+	const removeFields = [ 'sort' ];
+	removeFields.forEach((val) => delete reqQuery[val]);
+	let queryStr = JSON.stringify(reqQuery);	
+	queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
 	try {
-		//JSON.parse(queryStr)
-		const posts = await Post.find(JSON.parse(queryStr));
+		//find all the posts with that criteria
+		console.log(JSON.parse(queryStr));
+		const test = await Post.find(JSON.parse(queryStr));
+		//pagination
+		//.sort({price:-1})
+		let posts = Post.find(JSON.parse(queryStr)).sort(x)
+		
+		const page = parseInt(req.query.page) || 1;
+		const pageSize = parseInt(req.query.limit) || 25;
+		const skip = (page - 1) * pageSize;
+		const total = test.length; //trick with test so error query already.... wont hit
+		const pages = Math.ceil(total / pageSize);
+		posts = posts.skip(skip).limit(pageSize);
+		const aggelies = await posts;
+
 		res.send({
-			count: posts.length,
-			aggelies: posts
+			countPerPage: aggelies.length,
+			pages,
+			page,
+			aggelies: aggelies
+			//POSTMAN TEST --> localhost:4000/post?page=<page number>
 		});
 	} catch (error) {
+		console.log(error);
 		res.status(500).send(error);
 	}
 });
 
 //##########################     GET A POST  ##################
 
-// router.get('/:id', async (req, res) => {
-// 	try {
-// 		const post = await Post.findOne({ _id: req.params.id });
-
-// send picture
-// res.set('Content-Type', 'image/jpg');
-// res.send(post.image);
-// 		res.send(post);
-
-// 	} catch (error) {
-// 		res.status(500).send({ message: 'User not found' });
-// 	}
-// });
 router.get('/:id', async (req, res) => {
 	try {
 		const post = await Post.findOne({ _id: req.params.id });
-		
-		//send photo to browser
-		// res.set('Content-Type', 'image/jpg');
-		// res.send(post.image);
-		res.send({post});
-		
+
+//send picture
+ res.set('Content-Type', 'image/jpg');
+	res.send(post.image);
+	
 	} catch (error) {
 		res.status(500).send({ message: 'User not found' });
 	}
 });
-
 
 
 //##########################     DELETE A POST  ##################
