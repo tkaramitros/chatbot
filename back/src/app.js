a7dea704706adb9cf9c70a4794ff0d0ce5d8a95c
poi23 <tpoiras@gmail.com>
2022-04-02 23:54:33 +0300

    add JWT authentication

--- back/src/app.js
+++ back/src/app.js
@@ -19,9 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors());
 
-
-
-
 // app.use((req, res, next) => {
 // 	res.header('Access-Control-Allow-Origin', '*');
 // 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
