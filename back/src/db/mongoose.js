a0876ebc1dc963e7def97f53aa55121183b8510c
Vangelis Mazis <75184007+mazhs1992@users.noreply.github.com>
2022-04-11 21:41:54 +0300

    Update mongoose.js

--- back/src/db/mongoose.js
+++ back/src/db/mongoose.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
+const config = require('../config/keys');
+
+
+const mongoURI = config.mongoURI
 
 //const generateData = require("../test/test");
 const generateData = require("../test/testhome");
 //const generateData = require("../test/testoffice");
 //const generateData = require("../test/testland");
 
-mongoose.connect(
-  "mongodb+srv://admin:admin@chatbot-test-shared.48emm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if(err) console.log(err) 
-    else console.log("Connected to Database");
-    //generateData(2) //-->> to generate fake data
+mongoose.connect(  mongoURI,  { useNewUrlParser: true, useUnifiedTopology: true },  () => {
+    console.log("Connected to Database");
+  // generateData(1) //-->> to generate fake data
   }
 );
